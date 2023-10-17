2bf1f8f005c53483913e181aca6de2ffc56b7db3
serena-peschiaroli <serena.peschiaroli@gmail.com>
2023-10-17 16:04:13 +0200

    Add: prompt WIP

--- js/script.js
+++ js/script.js
@@ -1,6 +1,26 @@
-const buttonOne = document.querySelector(`button`);
-buttonOne.addEventListener(`click`, function() {
-    const userInput = prompt (`Inserisci il tuo nome`);
-    console.log (userInput);
 
+const generate = document.querySelector(`button`);
+generate.addEventListener(`click`, function() {
+    const userName = prompt (`Inserisci il tuo nome`);
+    const userSurname = prompt (`Inserisci il tuo cognome`);
+    const userColor = prompt (`Inserisci il tuo colore preferito`)
+
+    console.log (userName + userSurname + userColor);
+
+});
+
+
+const passwordTxt = document.getElementById("psw-gen");
+
+
+
+
+document.getElementById("pwd-gen").value = userName + userSurname + userColor;
+
+
+const copy = document.getElementById("copy");
+copy.addEventListener("click", () => {
+  passwordTxt.select();
+  document.execCommand("copy");
+  alert("Password Copied");
 });
