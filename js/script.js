be93dd332f793a9171319c2f3a0a738ffd3ff438
serena-peschiaroli <serena.peschiaroli@gmail.com>
2023-10-17 15:10:09 +0200

    Add: project scaffholding

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const buttonOne = document.querySelector(`button`);
+buttonOne.addEventListener(`click`, function() {
+    const userInput = prompt (`Inserisci il tuo nome`);
+    console.log (userInput);
+
+});
